e50e31934cdd2da81409276477b1d01de5fa95f5
EduardoSanjines <eduardosanjines@gmail.com>
2018-07-06 08:49:21 +0200

    Fix

---
+++ PooVueling/Calculadora.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooVueling
+{
+    /// <summary>
+    /// Class of the Calculadora
+    /// </summary>
+    /// <seealso cref="PooVueling.ICalculadora" />
+    /// <seealso cref="System.ICloneable" />
+    public class Calculadora : ICalculadora
+    {
+        /// <summary>
+        /// División de num1 y num2.
+        /// </summary>
+        /// <param name="num1">The num1.</param>
+        /// <param name="num2">The num2.</param>
+        /// <returns>Resultado de la division</returns>
+        public int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        /// <summary>
+        /// Multiplicacions the specified num1.
+        /// </summary>
+        /// <param name="num1">The num1.</param>
+        /// <param name="num2">The num2.</param>
+        /// <returns>Resultado de la multiplicación</returns>
+        public int Multiplicacion(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        /// <summary>
+        /// Restas the specified num1.
+        /// </summary>
+        /// <param name="num1">The num1.</param>
+        /// <param name="num2">The num2.</param>
+        /// <returns></returns>
+        public int Resta(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        /// <summary>
+        /// Sumas the specified num1.
+        /// </summary>
+        /// <param name="num1">The num1.</param>
+        /// <param name="num2">The num2.</param>
+        /// <returns></returns>
+        public int Suma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+    }
+}
---
+++ PooVueling/ICalculadora.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooVueling
+{
+    /// <summary>
+    /// Interface Calculadora
+    /// </summary>
+    public interface ICalculadora
+    {
+        /// <summary>
+        /// Sumas the specified num1 and num2.
+        /// </summary>
+        /// <param name="num1">The num1.</param>
+        /// <param name="num2">The num2.</param>
+        /// <returns></returns>
+        int Suma(int num1, int num2);
+        /// <summary>
+        /// Restas the specified num1 and num2.
+        /// </summary>
+        /// <param name="num1">The num1.</param>
+        /// <param name="num2">The num2.</param>
+        /// <returns></returns>
+        int Resta(int num1, int num2);
+        /// <summary>
+        /// Multiplicacions the specified num1 and num2.
+        /// </summary>
+        /// <param name="num1">The num1.</param>
+        /// <param name="num2">The num2.</param>
+        /// <returns></returns>
+        int Multiplicacion(int num1, int num2);
+        /// <summary>
+        /// Divisions the specified num1 and num2.
+        /// </summary>
+        /// <param name="num1">The num1.</param>
+        /// <param name="num2">The num2.</param>
+        /// <returns></returns>
+        int Division(int num1, int num2);
+    }
+}
